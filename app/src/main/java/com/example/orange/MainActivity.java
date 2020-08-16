16c5d6674d07182c9c2fff52ec9d2bf6aee5a420
Sama Tarek <47323089+SamaTarek@users.noreply.github.com>
2020-08-16 11:07:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/orange/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.orange;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button location;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        location =(Button)findViewById(R.id.locationbtn);
+
+
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==location)
+            startActivity(new Intent(this,Location.class));
+    }
+
+    }
+
