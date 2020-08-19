620f0d5a1b462e1c610a210ca5b142fcdcbfc68a
marwahmh <marwahisham@aucegypt.edu>
2020-08-19 05:18:20 +0200

    added sign in and up activities / buttons are now working

--- app/src/main/java/com/example/orange/MainActivity.java
+++ app/src/main/java/com/example/orange/MainActivity.java
@@ -5,27 +5,64 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import com.example.orange.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
 
     private Button location;
+    private Button login;
+    private Button sign_up;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        location =(Button)findViewById(R.id.locationbtn);
+        location = (Button) findViewById(R.id.locationbtn);
+        login = (Button) findViewById(R.id.button1);
+        sign_up = (Button) findViewById(R.id.button2);
 
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLocActiv();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openLoginActiv();
+            }
+        });
+
+        sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openSignActiv();
+            }
+        });
 
 
     }
 
-    @Override
-    public void onClick(View v) {
-        if(v==location)
-            startActivity(new Intent(this, LocationActivity.class));
+    //@Override
+    //public void onClick(View v) {
+    public void openLocActiv() {
+        Intent intent = new Intent(this, LocationActivity.class);
+        startActivity(intent);
+    }
+    public void openLoginActiv() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+    public void openSignActiv()
+    {
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 
-    }
+}
 
